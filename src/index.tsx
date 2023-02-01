66a630514f9305a6826645d3a975afe75556ab01
Jaciel Villalobos <jjacielv@gmail.com>
2023-02-01 17:40:25 -0600

    rename

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { App } from "./app";
 import reportWebVitals from "./reportWebVitals";
 import { ApolloProvider } from "@apollo/client";
 
